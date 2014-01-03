ec2d3035c3fafe7d61dc8b95637b83383b28f652
hataketsu <hataketsu@yahoo.com>
2014-01-04 04:31:48 +0700

    0.2 stable

--- mainos/lib.s
+++ mainos/lib.s
@@ -1,10 +1,8 @@
 segment .text
 global put_ch
 put_ch:
-	pop bp
-	pop ax
-	push bp
-	;mov al,'d'
+
+	mov al,'d'
 	mov ah,0xe
 	int 10h
 	ret
--- mainos/main.c
+++ mainos/main.c
@@ -1,8 +1,8 @@
 
 __asm__(".code16\n");
 __asm__("jmpl $0x0000, $main\n");
-void put_ch(char);
+void put_ch();
 void main() {
-	put_ch('r');
+	put_ch();
      
 }
